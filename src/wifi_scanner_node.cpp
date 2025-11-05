e90617af1107e853c0f8eaa9b0bd3131e26b1810
Haz25 <hasbro425@postech.ac.kr>
2025-11-05 10:17:12 +0900

    added freq

--- src/wifi_scanner_node.cpp
+++ src/wifi_scanner_node.cpp
@@ -13,6 +13,7 @@ using wifi_scanner::msg::WifiTop;
 struct AP {
   std::string ssid;
   std::string bssid;
+  int freq;
   int power;
 };
 
@@ -25,7 +26,7 @@ static bool isLAA(const std::string& bssid) {
 static std::vector<AP> run_scan() {
   std::vector<AP> v;
 
-  const char* cmd = "/usr/bin/env LC_ALL=C /usr/bin/nmcli --colors no -t -f SSID,SIGNAL,BSSID dev wifi 2>/dev/null";
+  const char* cmd = "/usr/bin/env LC_ALL=C /usr/bin/nmcli --colors no -t -f SSID,BSSID,FREQ,SIGNAL dev wifi 2>/dev/null";
   std::string out;
   std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
   char buf[1024];
@@ -38,17 +39,20 @@ static std::vector<AP> run_scan() {
     for (size_t pos = 0; (pos = line.find("\\:", pos)) != std::string::npos; )
       line.replace(pos, 2, "");
 
-    int sep1 = -1, sep2 = -1;
+    int sep1 = -1, sep2 = -1, sep3 = -1;
     for (int i=0; i<(int)line.size(); ++i) {
       if (line[i] != ':') continue;
-      if (sep1 < 0) { sep1 = i; }
-      else { sep2 = i; break; }
+
+      if (sep1 < 0) sep1 = i;
+      else if (sep2 < 0) sep2 = i;
+      else if (sep3 < 0) sep3 = i;
     }
 
     AP ap;
     ap.ssid = line.substr(0, sep1);
-    ap.bssid = line.substr(sep2+1);
-    ap.power = std::stoi(line.substr(sep1+1, sep2-sep1-1)) / 5 * 5;
+    ap.bssid = line.substr(sep1+1, sep2-sep1-1);
+    ap.freq = std::stoi(line.substr(sep2+1, sep3-sep2-1));
+    ap.power = std::stoi(line.substr(sep3+1)) / 5 * 5;
     v.push_back(ap);
   }
 
@@ -73,19 +77,21 @@ private:
     for (auto& a : aps) {
       s.ssid.push_back(a.ssid);
       s.bssid.push_back(a.bssid);
+      s.freq.push_back(a.freq);
       s.power.push_back(a.power);
     }
     pub_scan_->publish(s);
 
     sort(aps.begin(), aps.end(), [](AP &ap1, AP &ap2){
-        return ap1.power != ap2.power? ap1.power > ap2.power: ap1.ssid < ap2.ssid;
+        return ap1.power != ap2.power? ap1.power > ap2.power: ap1.freq != ap2.freq? ap1.freq > ap2.freq: ap1.ssid < ap2.ssid;
     });
 
     WifiTop t; t.stamp = s.stamp;
     for (auto& a : aps) {
       if (!isLAA(a.bssid)) {
-        t.ssid = a.ssid;
+        t.ssid  = a.ssid;
         t.bssid = a.bssid;
+        t.freq  = a.freq;
         t.power = a.power;
         pub_top_->publish(t);
         return;
